b2391275e761a1989f970817f7e61bce927a21f5
Francisca <fvargasserani@gmail.com>
2021-04-28 22:10:49 -0400

    got all the names finallyyyyyyyyy

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
 $(document).ready(function () {
     pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/',
-        pokemon = this.value,
         $.ajax(pokeApiUrl, {
             success: function (response) {
                 response.results.forEach(function (data) {
-                    $('#name').text(data.name);
-                    $('#pokemon-info').click(function () {
+                    $('#name').append('<p>' + data.name + '</p>');
+                    $('#pokemon-info').append("<button id='pokemon-info'>I'd like to know more about this pokemon!</button>").click(function () {
                         top.location.href = data.url
-                        $('#more-pokemons').click(function () {
-                            top.location.href = data.next
-                        })
                     })
-
+                    $('#more-pokemons').click(function () {
+                        top.location.href = data.next
+                    })
                 })
             }
         })
