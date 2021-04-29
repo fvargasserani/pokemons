36641263b3fabbd9744987a6896184df238cfef3
Francisca <fvargasserani@gmail.com>
2021-04-28 21:50:17 -0400

    almost dynamic results

--- js/app.js
+++ js/app.js
@@ -2,13 +2,16 @@ $(document).ready(function () {
     pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/',
         pokemon = this.value,
         $.ajax(pokeApiUrl, {
-            success: function (data) {
-                $('#pokemon-list').text(data.results)
-                $('#pokemon-info').click(function () {
-                    top.location.href = pokeApiUrl + pokemon + '/'
-                    $('#more-pokemons').click(function () {
-                        top.location.href = data.next
+            success: function (response) {
+                response.results.forEach(function (data) {
+                    $('#name').text(data.name);
+                    $('#pokemon-info').click(function () {
+                        top.location.href = data.url
+                        $('#more-pokemons').click(function () {
+                            top.location.href = data.next
+                        })
                     })
+
                 })
             }
         })
